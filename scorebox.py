8b589c20795c5f32e1891c0594445038b2f41061
Matt Selph <matt.selph@gmail.com>
2017-02-01 15:26:28 -0500

    Added matchup to box score; improved formatting

--- scorebox.py
+++ scorebox.py
@@ -22,15 +22,16 @@ class Scorebox(object):
         # set the total width of the box
         if len(self.nugget) == 0:
             self.totalwidth = 26
-            self.nugget = ' ' * 26
+            self.nugget = u'\u2573' * self.totalwidth
         else:
             self.totalwidth = len(self.nugget)
 
-        top = u'\u250c' + u'\u2500' * self.totalwidth + u'\u2510' + '\n'
-        visitor = u'\u2502' + self.vTeam + self.vScore.rjust(self.totalwidth - 3) + u'\u2502' + '\n'
-        home = u'\u2502' + self.hTeam + self.hScore.rjust(self.totalwidth - 3) + u'\u2502' + '\n'
-        divider = u'\u251c' + u'\u2500' * self.totalwidth + u'\u2524' + '\n'
-        nugget = u'\u2502' + self.nugget + u'\u2502' + '\n'
-        bottom = u'\u2514' + u'\u2500' * self.totalwidth + u'\u2518' + '\n'
+        top = u'\u2554' + u'\u2550' * self.totalwidth + u'\u2557' + '\n'
+        matchup = u'\u2551' + (self.vTeam + '@' + self.hTeam).rjust((self.totalwidth+7)/2) + u'\u2551'.rjust((self.totalwidth-3)/2) + '\n'
+        visitor = u'\u2551' + self.vTeam + self.vScore.rjust(self.totalwidth - 3) + u'\u2551' + '\n'
+        home = u'\u2551' + self.hTeam + self.hScore.rjust(self.totalwidth - 3) + u'\u2551' + '\n'
+        divider = u'\u255f' + u'\u2500' * self.totalwidth + u'\u2562' + '\n'
+        nugget = u'\u2551' + self.nugget + u'\u2551' + '\n'
+        bottom = u'\u255a' + u'\u2550' * self.totalwidth + u'\u255d' + '\n'
 
-        self.scorebox = top + visitor + home + divider + nugget + bottom
+        self.scorebox = top + matchup + visitor + home + divider + nugget + bottom
