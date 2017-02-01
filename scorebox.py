c467d5daf508d9a36c06f70522bec7b967c26789
Matt Selph <matt.selph@gmail.com>
2017-02-01 14:48:46 -0500

    Added game nugget to table

--- scorebox.py
+++ scorebox.py
@@ -22,12 +22,15 @@ class Scorebox(object):
         # set the total width of the box
         if len(self.nugget) == 0:
             self.totalwidth = 26
+            self.nugget = ' ' * 26
         else:
             self.totalwidth = len(self.nugget)
 
         top = u'\u250c' + u'\u2500' * self.totalwidth + u'\u2510' + '\n'
         visitor = u'\u2502' + self.vTeam + self.vScore.rjust(self.totalwidth - 3) + u'\u2502' + '\n'
         home = u'\u2502' + self.hTeam + self.hScore.rjust(self.totalwidth - 3) + u'\u2502' + '\n'
+        divider = u'\u251c' + u'\u2500' * self.totalwidth + u'\u2524' + '\n'
+        nugget = u'\u2502' + self.nugget + u'\u2502' + '\n'
         bottom = u'\u2514' + u'\u2500' * self.totalwidth + u'\u2518' + '\n'
 
-        self.scorebox = top + visitor + home + bottom
+        self.scorebox = top + visitor + home + divider + nugget + bottom
