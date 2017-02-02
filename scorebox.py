f5f1bdca9dbfd9b9c183b79a5ee40301ee85f793
Matt Selph <matt.selph@gmail.com>
2017-02-02 09:39:25 -0500

    Boxes are now the same size; added right justify fix

--- scorebox.py
+++ scorebox.py
@@ -2,7 +2,7 @@ class Scorebox(object):
     """Creates a box score for NBA
     """
 
-    def __init__(self, vTeam, vScore, hTeam, hScore, nugget):
+    def __init__(self, vTeam, vScore, hTeam, hScore, nugget, totalwidth):
         """Initialize a new Scorebox object.
 
         Args:
@@ -11,6 +11,7 @@ class Scorebox(object):
             hTeam (str): Home team's triCode
             hScore (str): Home team's score
             nugget (str): Game nugget
+            totalwidth(int): Width of the box scores
         """
 
         self.vTeam = vTeam
@@ -18,23 +19,24 @@ class Scorebox(object):
         self.hTeam = hTeam
         self.hScore = hScore
         self.nugget = nugget
+        self.totalwidth = totalwidth
 
         # set the total width of the box
         if len(self.nugget) == 0:
-            self.totalwidth = 26
             self.nugget = u'\u2573' * self.totalwidth
+
+        # fix the right justify variable if the nugget is shorter than the totalwidth
+        if len(self.nugget) < self.totalwidth:
+            rjust = (self.totalwidth-len(self.nugget)) + 1
         else:
-            self.totalwidth = len(self.nugget)
-
-        # highlight the winner
-
+            rjust = self.totalwidth-len(self.nugget)
 
         top = u'\u2554' + u'\u2550' * self.totalwidth + u'\u2557' + '\n'
-        matchup = u'\u2551' + (self.vTeam + '@' + self.hTeam).rjust((self.totalwidth+7)/2) + u'\u2551'.rjust(self.totalwidth-((self.totalwidth+7)/2)+1) + '\n'
+        matchup = u'\u2551' + (self.vTeam + '@' + self.hTeam).rjust((self.totalwidth+7)/2) + u'\u2551'.rjust((self.totalwidth-((self.totalwidth+7)/2))+1) + '\n'
         visitor = u'\u2551' + self.vTeam + self.vScore.rjust(self.totalwidth - 3) + u'\u2551' + '\n'
         home = u'\u2551' + self.hTeam + self.hScore.rjust(self.totalwidth - 3) + u'\u2551' + '\n'
         divider = u'\u255f' + u'\u2500' * self.totalwidth + u'\u2562' + '\n'
-        nugget = u'\u2551' + self.nugget + u'\u2551' + '\n'
+        nugget = u'\u2551' + self.nugget + u'\u2551'.rjust(rjust) + '\n'
         bottom = u'\u255a' + u'\u2550' * self.totalwidth + u'\u255d' + '\n'
 
         self.scorebox = top + matchup + visitor + home + divider + nugget + bottom
