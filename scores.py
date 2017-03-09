8ec33d0433613ef37b9dc9d90e991f58a1ef9201
Matt Selph <matt.selph@gmail.com>
2017-03-09 15:28:17 -0500

    Fixed issue where not having a game nugget messed up formatting

--- scores.py
+++ scores.py
@@ -22,15 +22,13 @@ def get_scores(dt):
     if num_games == '0':
         return "No games played on this date."
 
-    games = list()
-
     # all boxes should be the same size, represented by the length
     # of the longest game nugget
     nuggets = list()
     for g in datadict['games']:
         nuggets.append(len(str(g['nugget']['text']).strip()))
 
-    totalwidth = max(nuggets)
+    totalwidth = max(nuggets) or 40
     boxes = list()
 
     for g in datadict['games']:
